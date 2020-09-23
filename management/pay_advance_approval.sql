dbbd31910682ebea48c73af7a4603cbb57f9efc5
a531227909 <531227909@qq.com>
2020-09-24 02:18:39 +0800

    更新审核流程规则

--- management/pay_advance_approval.sql
+++ management/pay_advance_approval.sql
@@ -1,7 +1,9 @@
 DROP TABLE IF EXISTS pay_advance_approval;
 CREATE TABLE pay_advance_approval (
   `payAdvanceApprovalId` int auto_increment PRIMARY KEY COMMENT '预支审核权限自增ID',
-  `user_id` varchar(16) NOT NULL COMMENT '用户ID',
+  `user_id` varchar(16) NOT NULL DEFAULT 0 COMMENT '用户ID',
+  `user_type_id` varchar(16) NOT NULL DEFAULT 0 COMMENT '部门ID',
+  `type` varchar(16) NOT NULL DEFAULT 1 COMMENT '1:管理员 2:部门',
   `company_id` varchar(16) NOT NULL COMMENT '公司ID',
   `parentId` varchar(16) NOT NULL COMMENT '上级审核权限ID 0:没有权限 -1:顶级'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
--- management/user_type.sql
+++ management/user_type.sql
@@ -2,10 +2,11 @@ DROP TABLE IF EXISTS user_type;
 CREATE TABLE user_type (
   `user_type_id` int auto_increment PRIMARY KEY COMMENT '用户自增ID',
   `user_type_name` varchar(16) NOT NULL COMMENT '用户类型(岗位)名称',
-  `status` varchar(4) NOT NULL DEFAULT '1' COMMENT '状态 1:管理员 2:用户'
+  `status` varchar(4) NOT NULL DEFAULT '1' COMMENT '身份ID 0:超级管理员 1:用户 2:管理员'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-insert into user_type (user_type_name, status) values ('超级管理员','1');
+insert into user_type (user_type_name, status) values ('超级管理员','0');
+insert into user_type (user_type_name, status) values ('用户','1');
 insert into user_type (user_type_name, status) values ('驻场','2');
 insert into user_type (user_type_name, status) values ('人事','2');
 insert into user_type (user_type_name, status) values ('自营招聘员','2');
